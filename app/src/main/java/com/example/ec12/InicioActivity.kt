d563a5978721189f9660044b478ee627a5b3ec8c
Joaquin Rodriguez <joaquinrs20181659@gmail.com>
2022-05-20 02:14:22 -0500

    Hice la 1 Falta pulir

---
+++ app/src/main/java/com/example/ec12/InicioActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ec12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class InicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+
+        val data = getIntent().getStringExtra("NOMBRE")
+        Log.i("Inicio",data.toString())
+    }
+}
--- app/src/main/java/com/example/ec12/MainActivity.kt
+++ app/src/main/java/com/example/ec12/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.ec12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private var mEteID : EditText? = null
+    private var mButlog : Button? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //obtener id
+        mEteID = findViewById(R.id.mEteID)
+        mButlog= findViewById(R.id.mButLog)
+
+        mButlog!!.setOnClickListener { v: View ->
+            val nombre  : String = mEteID!!.text.toString()
+            intent.setClass(this,InicioActivity::class.java)
+            val data = Bundle()
+            data.putString("NOMBRE",nombre)
+            intent.putExtras(data)
+
+            startActivity(intent)
+
+        }
     }
 }
