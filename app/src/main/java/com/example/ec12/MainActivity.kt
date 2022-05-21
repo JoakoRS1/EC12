ccb27771eb2570fba8c227eb033f96e3b66e19ff
Gabriel <sam.perezf@hotmail.com>
2022-05-20 19:19:29 -0500

    navbar 2.0 nosotros 1.0 avanzando como el rayo

--- app/src/main/java/com/example/ec12/MainActivity.kt
+++ app/src/main/java/com/example/ec12/MainActivity.kt
@@ -3,8 +3,15 @@ package com.example.ec12
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mlMain: DrawerLayout
+    private lateinit var nvInicio: NavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,5 +19,26 @@ class MainActivity : AppCompatActivity() {
         val data = getIntent().getStringExtra("NOMBRE")
         Log.i("Inicio",data.toString())
 
+        mlMain = findViewById(R.id.dlMain)
+        nvInicio = findViewById(R.id.nvInicio)
+
+        nvInicio.setNavigationItemSelectedListener {
+            it.setChecked(true)
+
+            when(it.itemId){
+                R.id.iCartelera -> mostrarFragCartelera()
+                R.id.iNosotros -> mostrarFragNosotros()
+            }
+            mlMain.closeDrawers()
+            true
+        }
+    }
+
+    private fun mostrarFragCartelera() {
+        TODO("Not yet implemented")
+    }
+
+    private fun mostrarFragNosotros() {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/example/ec12/fragments/NosotrosFragment.kt
@@ -0,0 +1,4 @@
+package com.example.ec12.fragments
+
+class NosotrosFragment {
+}
