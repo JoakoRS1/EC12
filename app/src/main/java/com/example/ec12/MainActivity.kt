edc79ea1ea46968bd660d7845f66aaa61cd45c1d
Vane-byte <20180841@aloe.ulima.edu.pe>
2022-05-21 13:41:22 -0500

    Punto 3: Listado de peículas y listener

--- app/src/main/java/com/example/ec12/MainActivity.kt
+++ app/src/main/java/com/example/ec12/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentTransaction
+import com.example.ec12.fragments.CarteleraFragment
 import com.example.ec12.fragments.NosotrosFragment
 import com.google.android.material.navigation.NavigationView
 
@@ -14,13 +15,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nvInicio: NavigationView
 
     private val fragNosotros = NosotrosFragment()
+    private val fragCartelera = CarteleraFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val data = getIntent().getStringExtra("NOMBRE")
+        val data = intent.getStringExtra("NOMBRE")
         Log.i("Inicio",data.toString())
+        if (data!= null){
+            val bundle= Bundle()
+            bundle.putString("NOMBRE", data)
+            fragCartelera.arguments= bundle
+        }
+
 
         mlMain = findViewById(R.id.dlMain)
         nvInicio = findViewById(R.id.nvInicio)
@@ -41,13 +49,16 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+        val ft = supportFragmentManager.beginTransaction()
+        mostrarFragCartelera(ft)
+        ft.commit()
         //Cargar fragment por defecto
 
 
     }
 
     private fun mostrarFragCartelera(ft: FragmentTransaction) {
-        TODO("Not yet implemented")
+        ft.replace(R.id.fcvItems, fragCartelera)
     }
 
     private fun mostrarFragNosotros(ft: FragmentTransaction) {
